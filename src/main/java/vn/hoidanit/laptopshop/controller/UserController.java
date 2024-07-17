0c823a7edc9d3ef98aaa4554d266d138cc8862cc
Ngo Anh Tien <ngoanhtien902@gmail.com>
2024-07-17 13:38:37 +0700

    #55 -> #60. Aplly MVC model and Spring Data JPA with DI

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -2,19 +2,19 @@ package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.laptopshop.domain.User;
+// import vn.hoidanit.laptopshop.repository.UserRepository;
 import vn.hoidanit.laptopshop.service.UserService;
 
 @Controller
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
+    // private final UserRepository userRepository;
 
     public UserController(UserService userService) {
         this.userService = userService;
@@ -22,8 +22,7 @@ public class UserController {
 
     @RequestMapping("/")
     public String getHomePage(Model model) {
-        String test = this.userService.handleHello();
-        model.addAttribute("eric", test);
+        model.addAttribute("eric", "test");
         return "hello";
     }
 
@@ -34,8 +33,9 @@ public class UserController {
     }
 
     @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
-    public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit) {
-        System.out.println(" run here" + hoidanit);
+    public String createUserPage(Model model, @ModelAttribute("newUser") User user) {
+        System.out.println(" run here" + user);
+        this.userService.userHandleSave(user);
         return "hello";
     }
 
--- src/main/java/vn/hoidanit/laptopshop/service/UserService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/UserService.java
@@ -2,9 +2,24 @@ package vn.hoidanit.laptopshop.service;
 
 import org.springframework.stereotype.Service;
 
+import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.repository.UserRepository;
+
 @Service
 public class UserService {
-    public String handleHello() {
-        return "Hello from Service";
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    // public User userHandleSave(User user) {
+    // return this.userRepository.save(user);
+    // }
+
+    public User userHandleSave(User user) {
+        User u = this.userRepository.save(user);
+        System.out.println(u);
+        return u;
     }
 }
