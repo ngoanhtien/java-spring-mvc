ae2748e4529d6181078510c845d7e058069b13a5
Ngo Anh Tien <ngoanhtien902@gmail.com>
2024-08-10 23:56:55 +0700

    Load product into homepage and see detail of product

--- src/main/java/vn/ngotien/laptopshop/controller/client/HomePageController.java
+++ src/main/java/vn/ngotien/laptopshop/controller/client/HomePageController.java
@@ -1,12 +1,26 @@
 package vn.ngotien.laptopshop.controller.client;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import vn.ngotien.laptopshop.domain.Product;
+import vn.ngotien.laptopshop.service.ProductService;
 
 @Controller
 public class HomePageController {
+    private final ProductService productService;
+
+    public HomePageController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping("/")
-    public String getHomePage() {
+    public String getHomePage(Model model) {
+        List<Product> products = this.productService.getAllProducts();
+        model.addAttribute("products", products);
         return "client/homepage/show";
     }
 }
--- src/main/java/vn/ngotien/laptopshop/controller/client/ItemController.java
+++ src/main/java/vn/ngotien/laptopshop/controller/client/ItemController.java
@@ -6,12 +6,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import vn.ngotien.laptopshop.domain.Product;
+import vn.ngotien.laptopshop.service.ProductService;
 
 @Controller
 public class ItemController {
-    @GetMapping("/product/{id}")
-    public String getItemDetailPage(Model model, @PathVariable("id") long id) {
+    private final ProductService productService;
 
+    public ItemController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/product/detail/{id}")
+    public String getItemDetailPage(Model model, @PathVariable("id") long id) {
+        Product product = this.productService.getProductById(id);
+        model.addAttribute("product", product);
         return "client/product/detail";
     }
 }
